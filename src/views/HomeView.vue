59b2a4d746854ebe87683cd473d5d567657beb8b
xuxianyang <xxy56397@hotmail.com>
2024-04-29 17:08:33 +0800

    初始化项目

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    Welcome to Your Vue.js App
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
